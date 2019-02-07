f535b8d63c2046f2195f8986b9b8adeadfc46af8
segnolin <segnolin@gmail.com>
2019-02-07 13:35:26 +0800

    adjust OAuth 2.0 scope

--- download.py
+++ download.py
@@ -9,7 +9,7 @@ import os
 import sys
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/drive'
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
 
 def main():
     """
