cf5b47489c946111218ef6726f11f5a95bfc5fc4
segnolin <segnolin@gmail.com>
2019-10-13 19:36:26 +0800

    add selection to folders that have same name

--- download-2.py
+++ download-2.py
@@ -35,10 +35,40 @@ def main():
 
     folder = service.files().list(
             q="name='{}' and mimeType='application/vnd.google-apps.folder'".format(folder_name),
-            fields='files(id)').execute()
-    folder_id = folder['files'][0][u'id']
-    print folder_id
-    download_folder(service, folder_id, location, unicode(folder_name, 'utf-8'))
+            fields='files(id, name, parents)').execute()
+
+    total = len(folder['files'])
+    if total != 1:
+        print('{} folders found'.format(total))
+        prompt = 'Please select the folder you want to download:\n\n'
+        for i in range(total):
+            prompt += u'[{}]: {}\n'.format(i, get_full_path(service, folder['files'][i]))
+        prompt += '[{}]: All of above\n'.format(total)
+        print prompt
+        choice = int(raw_input('Your choice: '))
+        if choice == total:
+            for i in range(total):
+                folder_id = folder['files'][i]['id']
+                print('{} {}'.format(folder_id, folder_name))
+                download_folder(service, folder_id, location, unicode(folder_name, 'utf-8'))
+                print('')
+        elif choice < total:
+            folder_id = folder['files'][choice]['id']
+            print('{} {}'.format(folder_id, folder_name))
+            download_folder(service, folder_id, location, unicode(folder_name, 'utf-8'))
+    else:
+        folder_id = folder['files'][0]['id']
+        print('{} {}'.format(folder_id, folder_name))
+        download_folder(service, folder_id, location, unicode(folder_name, 'utf-8'))
+
+def get_full_path(service, folder):
+
+    files = service.files().get(fileId=folder['parents'][0], fields='id, name, parents').execute()
+    path = files['name'] + ' > ' + folder['name']
+    while files['name'] != 'My Drive':
+        files = service.files().get(fileId=files['parents'][0], fields='id, name, parents').execute()
+        path = files['name'] + ' > ' + path
+    return path
 
 def download_folder(service, folder_id, location, folder_name):
 
@@ -52,14 +82,14 @@ def download_folder(service, folder_id, location, folder_name):
             q="'{}' in parents".format(folder_id),
             fields='files(id, name, mimeType)').execute()
     result.extend(files['files'])
-    result = sorted(result, key=lambda k: k[u'name'])
+    result = sorted(result, key=lambda k: k['name'])
 
     total = len(result)
     current = 1
     for item in result:
-        file_id = item[u'id']
-        filename = item[u'name']
-        mime_type = item[u'mimeType']
+        file_id = item['id']
+        filename = item['name']
+        mime_type = item['mimeType']
         print file_id, filename, mime_type, '({}/{})'.format(current, total)
         if mime_type == 'application/vnd.google-apps.folder':
             download_folder(service, file_id, location, filename)
--- download.py
+++ download.py
@@ -34,10 +34,39 @@ def main():
 
     folder = service.files().list(
             q=f"name='{folder_name}' and mimeType='application/vnd.google-apps.folder'",
-            fields='files(id)').execute()
-    folder_id = folder['files'][0]['id']
-    print(folder_id)
-    download_folder(service, folder_id, location, folder_name)
+            fields='files(id, name, parents)').execute()
+
+    total = len(folder['files'])
+    if total != 1:
+        print(f'{total} folders found')
+        prompt = 'Please select the folder you want to download:\n\n'
+        for i in range(total):
+            prompt += f'[{i}]: {get_full_path(service, folder["files"][i])}\n'
+        prompt += f'[{total}]: All of above\n\nYour choice: '
+        choice = int(input(prompt))
+        if choice == total:
+            for i in range(total):
+                folder_id = folder['files'][i]['id']
+                print(f'{folder_id} {folder_name}')
+                download_folder(service, folder_id, location, folder_name)
+                print('')
+        elif choice < total:
+            folder_id = folder['files'][choice]['id']
+            print(f'{folder_id} {folder_name}')
+            download_folder(service, folder_id, location, folder_name)
+    else:
+        folder_id = folder['files'][0]['id']
+        print(f'{folder_id} {folder_name}')
+        download_folder(service, folder_id, location, folder_name)
+
+def get_full_path(service, folder):
+
+    files = service.files().get(fileId=folder['parents'][0], fields='id, name, parents').execute()
+    path = files['name'] + ' > ' + folder['name']
+    while files['name'] != 'My Drive':
+        files = service.files().get(fileId=files['parents'][0], fields='id, name, parents').execute()
+        path = files['name'] + ' > ' + path
+    return path
 
 def download_folder(service, folder_id, location, folder_name):
 
